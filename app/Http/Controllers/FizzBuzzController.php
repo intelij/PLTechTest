a43177917b7c288d2f9680ff4f7ba09a96d7f3d8
Khululekani Mkhonza <k.mkhonza@yahoo.co.uk>
2019-07-24 07:00:48 +0100

    Create controller and model

---
+++ app/Http/Controllers/FizzBuzzController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\FizzBuzz;
+use Illuminate\Http\Request;
+
+class FizzBuzzController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\FizzBuzz  $fizzBuzz
+     * @return \Illuminate\Http\Response
+     */
+    public function show(FizzBuzz $fizzBuzz)
+    {
+        //
+    }
+
+}
